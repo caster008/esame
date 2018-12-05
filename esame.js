fb69d60f0952a592165deb0be4f52d7b053613a9
caster008 <39692190+caster008@users.noreply.github.com>
2018-12-05 12:29:57 +0100

    Create esame.js

---
+++ esame.js
@@ -0,0 +1,58 @@
+var ToDo = [];
+var counter=0;
+function insert (name,description,completed,assignedTo)  {
+    id:counter++,
+    name:'A simple To Do',
+    description: 'To do my exam',
+    completed: false,
+    assignedTo: 'Caio'
+
+}  
+}
+return ToDo;
+ ToDo.push(ToDo);
+ return ToDo;
+
+function deleteToDo(id){
+    ToDo.id.splice(id,1);
+    return ToDo;
+    }
+var modifyStatus = function (id){
+    for (var i=0;i< list.lenght;i++){
+        if(list[i].id==id){
+            if(list.completed=false){
+                list[i].completed=true;
+            }
+        }
+    }
+} 
+var insert= (name,description,completed,assignedTo){
+    if(users=="Tizio" ||user=="Caio" ||user=="Sempronio" );
+    id:counter++,
+    name : name,
+    description:description,
+    completed: completed,
+    assignedTo: assignedTo
+}
+vargetUser=function(){
+    users==["Tizio" ||user=="Caio" ||user=="Sempronio" ];
+    return ToDo;
+}
+vargetUser("Adolfo","lavare l'auto?","true","adolfo");
+console.log(list);
+return ToDo;
+exports.readListbyUser=function(user){
+    var j=0;
+    for(var i=0;i<list.lenght;i++){
+        if(list[i].assigentTo==user){
+            readUsers[j]=list[i];
+            j++;
+        }
+    }
+    return readUsers;
+}
+exports.getList=function(){
+    return list;
+}
+this.insertToDo("Adolfo","lavare l'auto?","true","adolfo");
+console.log(list);
